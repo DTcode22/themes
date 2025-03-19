375bca4db6167dac70642ab5507f570baadb0645
Dejan <dejotuturu@gmail.com>
2025-03-19 04:50:32 +0100

    fix: theme and responsivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,17 @@ import BackgroundSelector from '../components/backgrounds/BackgroundSelector';
 export default function Home() {
   return (
     <div className="w-full h-full container mx-auto">
-      <div className="py-20 flex flex-col items-center justify-center">
-        <h1 className="text-primary text-5xl text-center font-bold mb-8">
+      <div className=" py-12 mx-4 flex flex-col items-center justify-center">
+        <h1 className="text-primary bg-primary-foreground p-4 rounded-lg text-4xl text-center font-bold">
           Multi Themes & Backgrounds
         </h1>
 
-        <div className="flex flex-col md:flex-row gap-8 items-center justify-center mb-16">
+        <div className="flex flex-col md:flex-row gap-6 items-center md:items-end justify-center mb-8">
           <div>
-            <h2 className="text-xl mb-4 text-center">Choose Theme</h2>
             <ThemeToggles />
           </div>
 
-          <div className="h-16 w-px bg-primary opacity-20 hidden md:block"></div>
+          <div className="h-32 w-px bg-primary opacity-20 hidden md:block"></div>
 
           <div>
             <h2 className="text-xl mb-4 text-center">Choose Background</h2>
@@ -23,7 +22,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mt-12 p-8 rounded-lg bg-primary-foreground text-primary border border-primary border-opacity-20 max-w-lg">
+        <div className="mt-8  p-8 rounded-lg bg-primary-foreground text-primary border border-primary border-opacity-20 max-w-lg">
           <h3 className="text-2xl mb-4">Demo Content</h3>
           <p className="mb-4">
             This demo showcases multiple themes and dynamic backgrounds for your
--- src/components/ThemeToggles.tsx
+++ src/components/ThemeToggles.tsx
@@ -1,15 +1,15 @@
-'use client'
+'use client';
 
-import { useTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 function ThemeToggles() {
-  const { theme, setTheme } = useTheme()
-  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   // The active theme is not available on the server.
   // If you have styling that is conditionally applied based on the active-theme,
   // you have to await the mounted state before rendering the active theme.
-  useEffect(() => setMounted(true), [])
+  useEffect(() => setMounted(true), []);
 
   const themeMapping: Record<string, string> = {
     light: 'Default',
@@ -17,23 +17,23 @@ function ThemeToggles() {
     tangerine: 'Tangerine',
     'dark-tangerine': 'Tangerine (dark)',
     mint: 'Mint',
-    'dark-mint': 'Mint (dark)'
-  }
+    'dark-mint': 'Mint (dark)',
+  };
 
   return (
     <div>
-      <div className="mt-16 grid grid-cols-3 grid-rows-2 grid-flow-col gap-4">
+      <div className="mt-16 grid grid-cols-3 grid-rows-2 grid-flow-col gap-2 md:gap-4">
         {Object.entries(themeMapping).map(([key, value]) => (
           <button
             key={key}
-            className={`px-4 py-2 font-semibold rounded-md transition-colors duration-200 ${
+            className={`md:px-4 py-2 font-semibold rounded-md transition-colors duration-200 ${
               // The theme is only available after the component is mounted.
               mounted && theme == key
                 ? 'border border-primary bg-primary-foreground text-primary'
                 : 'bg-primary text-primary-foreground'
             }`}
             onClick={() => {
-              setTheme(key)
+              setTheme(key);
             }}
           >
             {value}
@@ -41,7 +41,7 @@ function ThemeToggles() {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default ThemeToggles
+export default ThemeToggles;
